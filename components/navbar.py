de5239fc248050a22d0d7216b5f7c9d761d0a09f
whuang20226450 <winston78934546@gmail.com>
2023-06-05 13:55:43 +0800

    update main page datepicker and navbar.py

--- components/navbar.py
+++ components/navbar.py
@@ -14,36 +14,104 @@ users = list(df["user_id"].unique())
 # Define the navbar structure
 def Navbar():
 
-    layout = html.Div([
-        dbc.NavbarSimple(
-            children=[
-                dbc.NavItem(dbc.NavLink("Main Page", href="/main")),
-                dbc.NavItem(dbc.NavLink("Analysis", href="/analysis")),
-                dbc.NavItem(dbc.NavLink("Planning", href="/plan")),
-                dbc.NavItem(dbc.NavLink("Use", href="/use")),
-                dbc.NavItem(dbc.NavLink("Schedule suggestions", href="/suggest")),
-                html.Span(dcc.Dropdown(
-                            options=users,
-                            value="P0705",
-                            id = "user-dropdown",
-                            style={
-                                    'width': '150%'
-                                }
-                            ), 
-                        className="mr-auto"),
-            ] ,
-            brand = "Tempus",
-            brand_style = {"color": "white",
-                           "font-family": "Arial",
-                           "font-style": "italic",
-                           "font-weight": "bold",
-                           "font-size": "25px"
-                           },
-            brand_href = "/main",
-            color = "dark",
-            dark = True,
-            links_left= True
-        ), 
-    ])
+    # layout = html.Div([
+    #     dbc.NavbarSimple(
+    #         children=[
+    #             dbc.NavItem(dbc.NavLink("Main", href="/main")),
+    #             dbc.NavItem(dbc.NavLink("Analysis", href="/analysis")),
+    #             dbc.NavItem(dbc.NavLink("Plan", href="/plan")),
+    #             dbc.NavItem(dbc.NavLink("Use", href="/use")),
+    #             # dbc.NavItem(dbc.NavLink("Schedule suggestions", href="/suggest")),
+    #             html.Span(),
+    #             html.Span(
+    #                 dcc.Dropdown(
+    #                     options=users,
+    #                     value="P0705",
+    #                     id = "user-dropdown",
+    #                     style={
+    #                         'width': '150%',
+    #                         'background-color': '#f9f9f9',
+    #                     }
+    #                 ), 
+    #             ),
+                
+    #         ] ,
+    #         brand = "Tempus",
+    #         brand_style = {"color": "white",
+    #                        "font-family": "Arial",
+    #                        "font-style": "italic",
+    #                        "font-weight": "bold",
+    #                        "font-size": "28px"
+    #                        },
+    #         brand_href = "/main",
+    #         color = "dark",
+    #         dark = True,
+    #         links_left = True
+    #     ), 
+    # ])
+
+
+    layout = html.Div(
+        [
+            dbc.Navbar(
+                dbc.Container(
+                    [
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    html.A(
+                                        dbc.NavbarBrand(
+                                            "Tempus", 
+                                            className="ms-2",
+                                            style = {
+                                                "font-family": "Arial",
+                                                "font-style": "italic",
+                                                "font-weight": "bold",
+                                                "font-size": "25px"
+                                            },
+                                        ),
+                                        href="/main",
+                                        style={"textDecoration": "none"},
+                                    ),
+                                    width={"size": 1},
+                                ),
+                                dbc.Col(
+                                    [
+                                        dbc.Nav(
+                                            [
+                                                dbc.NavItem(dbc.NavLink("Main", href="/main")),
+                                                dbc.NavItem(dbc.NavLink("Analysis", href="/analysis")),
+                                                dbc.NavItem(dbc.NavLink("Plan", href="/plan")),
+                                                dbc.NavItem(dbc.NavLink("Use", href="/use")),
+                                            ]
+                                        ),
+                                    ],
+                                    width={"size": 3},
+                                ),
+                                dbc.Col(
+                                    dcc.Dropdown(
+                                        options=users,
+                                        value="P0705",
+                                        id = "user-dropdown",
+                                        style={
+                                            # 'width': '150%',
+                                            'background-color': '#E3F4F4',
+                                        }
+                                    ),
+                                    width={"size": 2, "offset": 6},
+                                ),
+                            ],
+                            align="center",
+                            # justify="between",
+                            style={'width': '100%'},
+                        ),                        
+                    ],
+                ),
+                color="dark",
+                dark=True,
+            ),
+        ],
+    )
+
 
     return layout
